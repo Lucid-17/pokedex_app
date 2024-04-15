b0bcb06e0a90ad3dc6fa60f2decb7ff08e7bbd3b
Louis Pate <lfpate11@gmail.com>
2024-04-15 19:44:37 -0400

    go and random buttons working. font also set and working. next step is getting numbered buttons to work

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,103 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PokemonData } from './models/pokemon.model';
+import { DataService } from './services/data.service';
+import { response } from 'express';
+import { PokemonEntryData } from './models/entry.model';
+import { max } from 'rxjs';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
+
 })
-export class AppComponent {
-  title = 'pokedex_app';
+
+
+export class AppComponent implements OnInit {
+
+
+  constructor(private dataService: DataService){
+
+  }
+
+  pokemonId: string | number = 'lapras';
+  pokemonData?: PokemonData;
+  pokemonEntry?: PokemonEntryData;
+  
+
+
+  ngOnInit(): void {
+    this.getPokemonInfo(this.pokemonId);
+    this.pokemonId = '';
+  }
+
+  onSubmit() {
+    this.getPokemonInfo(this.pokemonId);
+    this.pokemonId = '';
+    this.pokemonData!.types[1].type.name = '';
+  }
+
+  onRandom() {
+    this.pokemonId = Math.floor(Math.random() * (1025 - 1 + 1)) + 1;
+    this.getPokemonInfo(this.pokemonId);
+    this.pokemonId = '';
+    this.pokemonData!.types[1].type.name = '';
+  }
+
+    
+
+  private getPokemonInfo(pokemonName: string | number) {
+    this.dataService.getPokemonData(pokemonName)
+    .subscribe({
+      next: (response) => {
+        this.pokemonData = response;
+        console.log(response);
+      }
+    })
+    this.dataService.getPokemonEntry(pokemonName)
+    .subscribe({
+      next: (response_2) => {
+        this.pokemonEntry = response_2;
+        console.log(response_2)
+      }
+    })
+  }
+
 }
+
+
+
+// ngOnInit(): void {
+//   this.getWeatherData(this.cityName);
+//   this.cityName = '';   
+// }
+
+// onSubmit() {
+//   this.getWeatherData(this.cityName);
+//   this.cityName = '';
+// }
+
+// private getWeatherData(cityName: string) {
+//   this.WeatherService.getWeatherData(cityName)
+//   .subscribe({
+//     next: (response) => {
+//       this.weatherData = response;
+//       console.log(response);
+
+//     }
+//   });
+// }
+
+// title = 'weather-app-2';
+// }
+//   ngOnInitEntry(): void {
+//     this.dataService.getPokemonEntry('lapras')
+//     .subscribe({
+//       next: (response) => {
+//         this.pokemonEntry = response;
+//         console.log(response);
+//       }
+//     })
+//   }
+// }
--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,5 +1,10 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from '../environments/environment';
+import { PokemonData } from '../models/pokemon.model';
+import { PokemonEntryData } from '../models/entry.model';
+import { Observable } from 'rxjs';
+
 
 @Injectable({
   providedIn: 'root'
@@ -8,7 +13,15 @@ export class DataService {
 
   constructor(private http: HttpClient) { }
 
-  getPokemonData(pokemonName: string) {
-    this.http.get()
+  getPokemonData(pokemonId: string | number): Observable<PokemonData> {
+    return this.http.get<PokemonData>(environment.pokemonApiBaseUrl+'pokemon/'+pokemonId, {
+
+    })
+  }
+
+  getPokemonEntry(pokemonId: string | number): Observable<PokemonEntryData> {
+    return this.http.get<PokemonEntryData>(environment.pokemonApiBaseUrl+'pokemon-species/'+pokemonId, {
+
+    })
   }
 }
